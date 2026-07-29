7d8fb523ebfaaafef4402bdd83e04a6d1bdb22ad
Dhriti Ambasana <dhritiambasana@gmail.com>
2026-07-29 16:59:30 +0530

    Initial commit: manual LeetCode tracker

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ideal_sum = 0;
+        int vector_sum = 0;
+        int k = 0;
+
+        while (k <= nums.size()){
+            ideal_sum += k;
+            k++;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+            vector_sum += nums[i];
+        return ideal_sum - vector_sum;
+    }
+};

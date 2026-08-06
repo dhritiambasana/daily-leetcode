9908f59d46c239625c0906631aadf93fe182a57c
Dhriti Ambasana <dhritiambasana@gmail.com>
2026-08-06 18:25:09 +0530

    day 9

---
+++ array/744-next-greatest-letter/code.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        for (char c : letters)
+        {
+            if (c > target)
+                return c;
+        }
+        return letters[0];
+    }
+};
